df49c115f7450eb4df37be4ad45263b31c0a11e2
HalfS0ur <riveras.kendall99@gmail.com>
2024-03-01 15:10:57 -0600

    update CLA

--- sumadores/CLA/CLA_tb.py
+++ sumadores/CLA/CLA_tb.py
@@ -16,7 +16,7 @@ async def test_CLA(dut):
         resultado_esperado = (entrada_a + entrada_b + entrada_carry) & 0xFF
         logica_carry = 1 if entrada_a + entrada_b + entrada_carry > 256 else 0
         
-        await Timer(1, 'ns') 
+        await Timer(2, 'ns') 
 
         assert dut.sum.value == resultado_esperado, f"Sum output was incorrect. Got {dut.sum.value}, expected {resultado_esperado}"
         assert dut.c_out.value == logica_carry, f"Carry out output was incorrect. Got {dut.c_out.value}, expected {logica_carry}"

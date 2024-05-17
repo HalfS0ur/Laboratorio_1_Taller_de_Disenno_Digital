80dbc0da6f7f953c36d4e52462c09b492e6af218
HalfS0ur <riveras.kendall99@gmail.com>
2024-05-16 20:42:29 -0600

    Prueba que cubre todas las combinaciones

--- sumadores/FA/full_adder_tb.py
+++ sumadores/FA/full_adder_tb.py
@@ -3,21 +3,35 @@ import random
 from cocotb.triggers import Timer
 
 @cocotb.test()
-async def test_full_adder(dut):
-    """Prueba del módulo full adder"""
-    for run in range(16):
-        entrada_a = random.randint(0, 1)
-        dut.a.value = entrada_a
-        entrada_b = random.randint(0, 1)
-        dut.b.value = entrada_b
-        entrada_carry = random.randint(0, 1)
-        dut.cin.value = entrada_carry
-        
-        resultado_esperado = (entrada_a + entrada_b + entrada_carry) & 0b1
-        logica_carry = 1 if entrada_a + entrada_b + entrada_carry >= 2 else 0
-        
-        await Timer(1, 'ns') 
+async def prueba_full_adder_sin_cin(dut):
+    dut.cin.value = 0
 
-        assert dut.sum.value == resultado_esperado, f"Sum output was incorrect. Got {dut.sum.value}, expected {resultado_esperado}"
-        assert dut.cout.value == logica_carry, f"Carry out output was incorrect. Got {dut.cout.value}, expected {logica_carry}"
+    for valor_a in range(2):
+        dut.a.value = valor_a
+        for valor_b in range (2):
+            dut.b.value = valor_b
 
+            resultado = (valor_a + valor_b) & 0b1
+            carry_out = ((valor_a + valor_b) >> 1) & 0b1
+
+            await Timer(1, 'ns')
+
+            assert dut.sum.value == resultado, f"Sum output was incorrect. Got {dut.sum.value}, expected {resultado}"
+            assert dut.cout.value == carry_out, f"Carry out output was incorrect. Got {dut.cout.value}, expected {carry_out}"
+
+@cocotb.test()
+async def prueba_full_adder_con_cin(dut):
+    dut.cin.value = 1
+
+    for valor_a in range(2):
+        dut.a.value = valor_a
+        for valor_b in range (2):
+            dut.b.value = valor_b
+
+            resultado = (valor_a + valor_b + 1) & 0b1
+            carry_out = ((valor_a + valor_b + 1) >> 1) & 0b1
+
+            await Timer(1, 'ns')
+
+            assert dut.sum.value == resultado, f"Sum output was incorrect. Got {dut.sum.value}, expected {resultado}"
+            assert dut.cout.value == carry_out, f"Carry out output was incorrect. Got {dut.cout.value}, expected {carry_out}"

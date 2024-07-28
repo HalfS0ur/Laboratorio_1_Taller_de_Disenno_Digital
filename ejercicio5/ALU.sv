661118010d94773af07182561f1c4b3113b4a1ed
HalfS0ur <riveras.kendall99@gmail.com>
2024-07-27 18:31:22 -0600

    Formato puertos

--- ejercicio5/ALU.sv
+++ ejercicio5/ALU.sv
@@ -3,14 +3,14 @@
 module ALU #(
     parameter ANCHO = 4
 )(
-    input  logic signed [ANCHO-1:0]  ALUa_i,
-    input  logic signed [ANCHO-1:0]  ALUb_i,
-    input  logic                     ALUflagin_i,
-    input  logic        [3:0]        ALUcontrol_i,
+    input  logic signed [ANCHO-1:0]  ALUA,
+    input  logic signed [ANCHO-1:0]  ALUB,
+    input  logic                     ALUFlagIn,
+    input  logic        [3:0]        ALUControl,
     
-    output logic        [ANCHO-1:0]  ALUresult_o,
-    output logic                     ALUflags_o,
-    output logic                     zero_o
+    output logic        [ANCHO-1:0]  ALUResult,
+    output logic                     ALUFlags,
+    output logic                     ALUZero
     );
     
     logic [ANCHO:0] carry_suma;
@@ -19,100 +19,100 @@ module ALU #(
     logic [ANCHO-1:0] Ai = 0;
     logic [ANCHO-1:0] As = 0;
 
-    assign carry_suma = ALUa_i + ALUb_i + ALUflagin_i;
-    assign carry_resta = ALUa_i - ALUb_i - ALUflagin_i;
+    assign carry_suma = ALUA + ALUB + ALUFlagIn;
+    assign carry_resta = ALUA - ALUB - ALUFlagIn;
     
     always_comb begin
-        case(ALUcontrol_i)
+        case(ALUControl)
             4'h0: begin
-                    ALUresult_o = ALUa_i & ALUb_i; //and
-                    ALUflags_o = 0;
+                    ALUResult = ALUA & ALUB; //and
+                    ALUFlags = 0;
                   end
                   
             4'h1: begin
-                    ALUresult_o = ALUa_i | ALUb_i; //or
-                    ALUflags_o = 0;
+                    ALUResult = ALUA | ALUB; //or
+                    ALUFlags = 0;
                   end
                   
             4'h2: begin
-                    ALUresult_o = ALUa_i + ALUb_i + ALUflagin_i; //suma
-                    ALUflags_o = carry_suma [ANCHO];
+                    ALUResult = ALUA + ALUB + ALUFlagIn; //suma
+                    ALUFlags = carry_suma [ANCHO];
                   end
                   
             4'h3: begin //incremento en 1
-                    ALUflags_o = 0;
-                    if (ALUflagin_i == 0)
-                        ALUresult_o = ALUa_i + 1;
+                    ALUFlags = 0;
+                    if (ALUFlagIn == 0)
+                        ALUResult = ALUA + 1;
                     else
-                        ALUresult_o = ALUb_i + 1;
+                        ALUResult = ALUB + 1;
                   end
                   
             4'h4: begin //decremento en 1
-                    ALUflags_o = 0;
-                    if (ALUflagin_i == 0)
-                        ALUresult_o = ALUa_i - 1;
+                    ALUFlags = 0;
+                    if (ALUFlagIn == 0)
+                        ALUResult = ALUA - 1;
                     else
-                        ALUresult_o = ALUb_i - 1;
+                        ALUResult = ALUB - 1;
                   end
                   
             4'h5: begin //not
-                    ALUflags_o = 0;
-                    if (ALUflagin_i == 0)
-                        ALUresult_o = ~ALUa_i;
+                    ALUFlags = 0;
+                    if (ALUFlagIn == 0)
+                        ALUResult = ~ALUA;
                     else
-                        ALUresult_o = ~ALUb_i;
+                        ALUResult = ~ALUB;
                   end
                   
             4'h6: begin //resta
-                    ALUresult_o = ALUa_i - ALUb_i - ALUflagin_i;
-                    ALUflags_o = carry_resta [ANCHO];
+                    ALUResult = ALUA - ALUB - ALUFlagIn;
+                    ALUFlags = carry_resta [ANCHO];
                   end
                   
             4'h7: begin //xor
-                    ALUresult_o = ALUa_i ^ ALUb_i; 
-                    ALUflags_o = 0;
+                    ALUResult = ALUA ^ ALUB; 
+                    ALUFlags = 0;
                   end
                   
             4'h8: begin
-                    if (ALUflagin_i == 0)
-                        ALUresult_o = ALUa_i << ALUb_i;
+                    if (ALUFlagIn == 0)
+                        ALUResult = ALUA << ALUB;
                     else begin
-                        Ai = ~ALUa_i;
-                        As = Ai << ALUb_i;
-                        ALUresult_o = ~As;
+                        Ai = ~ALUA;
+                        As = Ai << ALUB;
+                        ALUResult = ~As;
                     end
                     
-                    if (ALUb_i <= ANCHO) //Logica para calcular el ultimo bit que sale
-                          ALUflags_o = ALUa_i[((ANCHO)-ALUb_i)];
+                    if (ALUB <= ANCHO) //Logica para calcular el ultimo bit que sale
+                          ALUFlags = ALUA[((ANCHO)-ALUB)];
                     else
-                          ALUflags_o = ALUflagin_i;
+                          ALUFlags = ALUFlagIn;
                   end
                   
             4'h9: begin
-                    if (ALUflagin_i == 0)
-                        ALUresult_o = ALUa_i >> ALUb_i;
+                    if (ALUFlagIn == 0)
+                        ALUResult = ALUA >> ALUB;
                     else begin
-                        Ai = ~ALUa_i;
-                        As = Ai >> ALUb_i;
-                        ALUresult_o = ~As;
+                        Ai = ~ALUA;
+                        As = Ai >> ALUB;
+                        ALUResult = ~As;
                     end
                     
-                    if (ALUb_i <= ANCHO) //Logica para calcular el ultimo bit que sale
-                         ALUflags_o = ALUa_i[ALUb_i-1];
+                    if (ALUB <= ANCHO) //Logica para calcular el ultimo bit que sale
+                         ALUFlags = ALUA[ALUB-1];
                     else
-                         ALUflags_o = ALUflagin_i;
+                         ALUFlags = ALUFlagIn;
                   end
                         
             default: begin
-                         ALUresult_o = 'bx;
-                         ALUflags_o = 'bx;
+                         ALUResult = 'bx;
+                         ALUFlags = 'bx;
                      end
         endcase
         
-        if (ALUresult_o == 0)
-            zero_o = 1;
+        if (ALUResult == 0)
+            ALUZero = 1;
         else
-            zero_o = 0;
+            ALUZero = 0;
     end
     
 endmodule
--- ejercicio5/testing/ALU_tb.py
+++ ejercicio5/testing/ALU_tb.py
@@ -8,247 +8,247 @@ valores_b = [0, 8, 12, 14, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15]
 @cocotb.test()
 async def test_AND(dut):
     '''Prueba de la operación AND'''
-    dut.ALUa_i.value = 0
-    dut.ALUb_i.value = 0
-    dut.ALUflagin_i.value = 0
-    dut.ALUcontrol_i.value = 0
+    dut.ALUA.value = 0
+    dut.ALUB.value = 0
+    dut.ALUFlagIn.value = 0
+    dut.ALUControl.value = 0
 
     for valor_a in range(RANGO):
-        dut.ALUa_i.value = valor_a
+        dut.ALUA.value = valor_a
         for valor_b in range(RANGO):
-            dut.ALUcontrol_i.value = 0
-            dut.ALUb_i.value = valor_b
+            dut.ALUControl.value = 0
+            dut.ALUB.value = valor_b
             await Timer(1, 'ns')
-            assert dut.ALUresult_o.value == valor_a & valor_b, f"El valor de la salida ALUresult_o es incorrecto. Se obtuvo {dut.ALUresult_o.value}, se esperaba {valor_a & valor_b}"
+            assert dut.ALUResult.value == valor_a & valor_b, f"El valor de la salida ALUResult es incorrecto. Se obtuvo {dut.ALUResult.value}, se esperaba {valor_a & valor_b}"
 
 @cocotb.test()
 async def test_OR(dut):
     '''Prueba de la operación OR'''
-    dut.ALUa_i.value = 0
-    dut.ALUb_i.value = 0
-    dut.ALUflagin_i.value = 0
-    dut.ALUcontrol_i.value = 1
+    dut.ALUA.value = 0
+    dut.ALUB.value = 0
+    dut.ALUFlagIn.value = 0
+    dut.ALUControl.value = 1
 
     for valor_a in range(RANGO):
-        dut.ALUa_i.value = valor_a
+        dut.ALUA.value = valor_a
         for valor_b in range(RANGO):
-            dut.ALUb_i.value = valor_b
+            dut.ALUB.value = valor_b
             await Timer(1, 'ns')
-            assert dut.ALUresult_o.value == valor_a | valor_b, f"El valor de la salida ALUresult_o es incorrecto. Se obtuvo {dut.ALUresult_o.value}, se esperaba {valor_a | valor_b}"
+            assert dut.ALUResult.value == valor_a | valor_b, f"El valor de la salida ALUResult es incorrecto. Se obtuvo {dut.ALUResult.value}, se esperaba {valor_a | valor_b}"
 
 @cocotb.test()
 async def prueba_suma_sin_carry(dut):
     '''Prueba de la operación suma sin carry in'''
-    dut.ALUa_i.value = 0
-    dut.ALUb_i.value = 0
-    dut.ALUflagin_i.value = 0
-    dut.ALUcontrol_i.value = 2
+    dut.ALUA.value = 0
+    dut.ALUB.value = 0
+    dut.ALUFlagIn.value = 0
+    dut.ALUControl.value = 2
 
     for valor_a in range(RANGO):
-        dut.ALUa_i.value = valor_a
+        dut.ALUA.value = valor_a
         for valor_b in range(RANGO):
-            dut.ALUb_i.value = valor_b
+            dut.ALUB.value = valor_b
             resultado_suma = (valor_a + valor_b) & 0xF
             resultado_carry = ((valor_b + valor_a) >> 4) & 1
             await Timer(1, 'ns')
-            assert dut.ALUresult_o.value == resultado_suma, f"El valor de la salida ALUresult_o es incorrecto. Se obtuvo {dut.ALUresult_o.value}, se esperaba {resultado_suma}"
-            assert dut.ALUflags_o.value == resultado_carry, f"El valor de la salida ALUflags_o es incorrecto. Se obtuvo {dut.ALUflags_o.value}, se esperaba {resultado_carry}"
+            assert dut.ALUResult.value == resultado_suma, f"El valor de la salida ALUResult es incorrecto. Se obtuvo {dut.ALUResult.value}, se esperaba {resultado_suma}"
+            assert dut.ALUFlags.value == resultado_carry, f"El valor de la salida ALUFlags es incorrecto. Se obtuvo {dut.ALUFlags.value}, se esperaba {resultado_carry}"
 
 @cocotb.test()
 async def prueba_suma_con_carry(dut):
     '''Prueba de la operación suma con carry in'''
-    dut.ALUa_i.value = 0
-    dut.ALUb_i.value = 0
-    dut.ALUflagin_i.value = 1
-    dut.ALUcontrol_i.value = 2
+    dut.ALUA.value = 0
+    dut.ALUB.value = 0
+    dut.ALUFlagIn.value = 1
+    dut.ALUControl.value = 2
 
     for valor_a in range(RANGO):
-        dut.ALUa_i.value = valor_a
+        dut.ALUA.value = valor_a
         for valor_b in range(RANGO):
-            dut.ALUb_i.value = valor_b
+            dut.ALUB.value = valor_b
             resultado_suma = (valor_a + valor_b + 1) & 0xF
             resultado_carry = ((valor_a + valor_b + 1) >> 4) & 1
             await Timer(1, 'ns')
-            assert dut.ALUresult_o.value == resultado_suma, f"El valor de la salida ALUresult_o es incorrecto. Se obtuvo {dut.ALUresult_o.value}, se esperaba {resultado_suma}"
-            assert dut.ALUflags_o.value == resultado_carry, f"El valor de la salida ALUflags_o es incorrecto. Se obtuvo {dut.ALUflags_o.value}, se esperaba {resultado_carry}"
+            assert dut.ALUResult.value == resultado_suma, f"El valor de la salida ALUResult es incorrecto. Se obtuvo {dut.ALUResult.value}, se esperaba {resultado_suma}"
+            assert dut.ALUFlags.value == resultado_carry, f"El valor de la salida ALUFlags es incorrecto. Se obtuvo {dut.ALUFlags.value}, se esperaba {resultado_carry}"
 
 @cocotb.test()
 async def test_incrementar_1(dut):
     '''Prueba de la operación incrementar en 1'''
-    dut.ALUa_i.value = 0
-    dut.ALUb_i.value = 0
-    dut.ALUflagin_i.value = 0
-    dut.ALUcontrol_i.value = 3
+    dut.ALUA.value = 0
+    dut.ALUB.value = 0
+    dut.ALUFlagIn.value = 0
+    dut.ALUControl.value = 3
 
     for valor_a in range (RANGO):
-        dut.ALUa_i.value = valor_a
-        dut.ALUflagin_i.value = 0
+        dut.ALUA.value = valor_a
+        dut.ALUFlagIn.value = 0
         resultado_a1 = (valor_a + 1) & 0xF
         await Timer(1, 'ns')
-        assert dut.ALUresult_o.value == resultado_a1, f"El valor de la salida ALUresult_o es incorrecto. Se obtuvo {dut.ALUresult_o.value}, se esperaba {resultado_a1}"
+        assert dut.ALUResult.value == resultado_a1, f"El valor de la salida ALUResult es incorrecto. Se obtuvo {dut.ALUResult.value}, se esperaba {resultado_a1}"
 
     for valor_b in range (RANGO):
-        dut.ALUb_i.value = valor_b
-        dut.ALUflagin_i.value = 1
+        dut.ALUB.value = valor_b
+        dut.ALUFlagIn.value = 1
         resultado_b1 = (valor_b + 1) & 0xF
         await Timer(1, 'ns')
-        assert dut.ALUresult_o.value == resultado_b1, f"El valor de la salida ALUresult_o es incorrecto. Se obtuvo {dut.ALUresult_o.value}, se esperaba {resultado_b1}"
+        assert dut.ALUResult.value == resultado_b1, f"El valor de la salida ALUResult es incorrecto. Se obtuvo {dut.ALUResult.value}, se esperaba {resultado_b1}"
 
 @cocotb.test()
 async def test_decrementar_1(dut):
     '''Prueba de la operación decrementar en 1'''
-    dut.ALUa_i.value = 0
-    dut.ALUb_i.value = 0
-    dut.ALUflagin_i.value = 0
-    dut.ALUcontrol_i.value = 4
+    dut.ALUA.value = 0
+    dut.ALUB.value = 0
+    dut.ALUFlagIn.value = 0
+    dut.ALUControl.value = 4
 
     for valor_a in range (RANGO):
-        dut.ALUa_i.value = valor_a
+        dut.ALUA.value = valor_a
         resultado_am1 = (valor_a - 1) & 0xF
         await Timer(1, 'ns')
-        assert dut.ALUresult_o.value == resultado_am1, f"El valor de la salida ALUresult_o es incorrecto. Se obtuvo {dut.ALUresult_o.value}, se esperaba {resultado_am1}"
+        assert dut.ALUResult.value == resultado_am1, f"El valor de la salida ALUResult es incorrecto. Se obtuvo {dut.ALUResult.value}, se esperaba {resultado_am1}"
 
     for valor_b in range (RANGO):
-        dut.ALUb_i.value = valor_b
-        dut.ALUflagin_i.value = 1
+        dut.ALUB.value = valor_b
+        dut.ALUFlagIn.value = 1
         resultado_bm1 = (valor_b - 1) & 0xF
         await Timer(1, 'ns')
-        assert dut.ALUresult_o.value == resultado_bm1, f"El valor de la salida ALUresult_o es incorrecto. Se obtuvo {dut.ALUresult_o.value}, se esperaba {resultado_bm1}"
+        assert dut.ALUResult.value == resultado_bm1, f"El valor de la salida ALUResult es incorrecto. Se obtuvo {dut.ALUResult.value}, se esperaba {resultado_bm1}"
 
 
 @cocotb.test()
 async def test_NOT(dut):
     '''Prueba de la operación NOT'''
-    dut.ALUa_i.value = 0
-    dut.ALUb_i.value = 0
-    dut.ALUflagin_i.value = 0
-    dut.ALUcontrol_i.value = 5
+    dut.ALUA.value = 0
+    dut.ALUB.value = 0
+    dut.ALUFlagIn.value = 0
+    dut.ALUControl.value = 5
 
     for valor_a in range (RANGO):
-        dut.ALUa_i.value = valor_a
+        dut.ALUA.value = valor_a
         resultado_not_a = ~valor_a & 0xF
         await Timer(1, 'ns')
-        assert dut.ALUresult_o.value == resultado_not_a, f"El valor de la salida ALUresult_o es incorrecto. Se obtuvo {dut.ALUresult_o.value}, se esperaba {resultado_not_a}"
+        assert dut.ALUResult.value == resultado_not_a, f"El valor de la salida ALUResult es incorrecto. Se obtuvo {dut.ALUResult.value}, se esperaba {resultado_not_a}"
 
     for valor_b in range (RANGO):
-        dut.ALUb_i.value = valor_b
-        dut.ALUflagin_i.value = 1
+        dut.ALUB.value = valor_b
+        dut.ALUFlagIn.value = 1
         resultado_not_b = ~valor_b & 0xF
         await Timer(1, 'ns')
-        assert dut.ALUresult_o.value == resultado_not_b, f"El valor de la salida ALUresult_o es incorrecto. Se obtuvo {dut.ALUresult_o.value}, se esperaba {resultado_not_b}"
+        assert dut.ALUResult.value == resultado_not_b, f"El valor de la salida ALUResult es incorrecto. Se obtuvo {dut.ALUResult.value}, se esperaba {resultado_not_b}"
 
 @cocotb.test()
 async def prueba_resta_sin_carry(dut):
     '''Prueba de la operación resta sin carry in'''
-    dut.ALUa_i.value = 0
-    dut.ALUb_i.value = 0
-    dut.ALUflagin_i.value = 0
-    dut.ALUcontrol_i.value = 6
+    dut.ALUA.value = 0
+    dut.ALUB.value = 0
+    dut.ALUFlagIn.value = 0
+    dut.ALUControl.value = 6
 
     for valor_a in range(RANGO):
-        dut.ALUa_i.value = valor_a
+        dut.ALUA.value = valor_a
         for valor_b in range(RANGO):
-            dut.ALUb_i.value = valor_b
+            dut.ALUB.value = valor_b
             resultado_resta = (valor_a - valor_b) & 0xF
             resultado_carry_resta = ((valor_a - valor_b) >> 4) & 1
             await Timer(1, 'ns')
-            assert dut.ALUresult_o.value == resultado_resta, f"El valor de la salida ALUresult_o es incorrecto. Se obtuvo {dut.ALUresult_o.value}, se esperaba {resultado_resta}"
-            assert dut.ALUflags_o.value == resultado_carry_resta, f"El valor de la salida ALUflags_o es incorrecto. Se obtuvo {dut.ALUflags_o.value}, se esperaba {resultado_carry_resta}"
+            assert dut.ALUResult.value == resultado_resta, f"El valor de la salida ALUResult es incorrecto. Se obtuvo {dut.ALUResult.value}, se esperaba {resultado_resta}"
+            assert dut.ALUFlags.value == resultado_carry_resta, f"El valor de la salida ALUFlags es incorrecto. Se obtuvo {dut.ALUFlags.value}, se esperaba {resultado_carry_resta}"
 
 @cocotb.test()
 async def prueba_resta_con_carry(dut):
     '''Prueba de la operación suma con carry in'''
-    dut.ALUa_i.value = 0
-    dut.ALUb_i.value = 0
-    dut.ALUflagin_i.value = 1
-    dut.ALUcontrol_i.value = 6
+    dut.ALUA.value = 0
+    dut.ALUB.value = 0
+    dut.ALUFlagIn.value = 1
+    dut.ALUControl.value = 6
 
     for valor_a in range(RANGO):
-        dut.ALUa_i.value = valor_a
+        dut.ALUA.value = valor_a
         for valor_b in range(RANGO):
-            dut.ALUb_i.value = valor_b
+            dut.ALUB.value = valor_b
             resultado_resta = (valor_a - valor_b - 1) & 0xF
             resultado_carry_resta = ((valor_a - valor_b - 1) >> 4) & 1
             await Timer(1, 'ns')
-            assert dut.ALUresult_o.value == resultado_resta, f"El valor de la salida ALUresult_o es incorrecto. Se obtuvo {dut.ALUresult_o.value}, se esperaba {resultado_resta}"
-            assert dut.ALUflags_o.value == resultado_carry_resta, f"El valor de la salida ALUflags_o es incorrecto. Se obtuvo {dut.ALUflags_o.value}, se esperaba {resultado_carry_resta}"
+            assert dut.ALUResult.value == resultado_resta, f"El valor de la salida ALUResult es incorrecto. Se obtuvo {dut.ALUResult.value}, se esperaba {resultado_resta}"
+            assert dut.ALUFlags.value == resultado_carry_resta, f"El valor de la salida ALUFlags es incorrecto. Se obtuvo {dut.ALUFlags.value}, se esperaba {resultado_carry_resta}"
 
 @cocotb.test()
 async def test_XOR(dut):
     '''Prueba de la operación XOR'''
-    dut.ALUa_i.value = 0
-    dut.ALUb_i.value = 0
-    dut.ALUflagin_i.value = 0
-    dut.ALUcontrol_i.value = 7
+    dut.ALUA.value = 0
+    dut.ALUB.value = 0
+    dut.ALUFlagIn.value = 0
+    dut.ALUControl.value = 7
 
     for valor_a in range (RANGO):
-        dut.ALUa_i.value = valor_a
+        dut.ALUA.value = valor_a
         for valor_b in range (RANGO):
-            dut.ALUb_i.value = valor_b
+            dut.ALUB.value = valor_b
             await Timer(1, 'ns')
-            assert dut.ALUresult_o.value == valor_a ^ valor_b, f"ALU output ALUresult_o was incorrect. Got {dut.ALUresult_o.value}, expected {(valor_a ^ valor_b)}"
+            assert dut.ALUResult.value == valor_a ^ valor_b, f"ALU output ALUResult was incorrect. Got {dut.ALUResult.value}, expected {(valor_a ^ valor_b)}"
 
 @cocotb.test()
 async def test_corrimiento_izquierda_ceros(dut):
-    dut.ALUa_i.value = 0
-    dut.ALUb_i.value = 0
-    dut.ALUflagin_i.value = 0
-    dut.ALUcontrol_i.value = 8
+    dut.ALUA.value = 0
+    dut.ALUB.value = 0
+    dut.ALUFlagIn.value = 0
+    dut.ALUControl.value = 8
 
     for valor_a in range (RANGO):
-        dut.ALUa_i.value = valor_a
+        dut.ALUA.value = valor_a
         for valor_b in range (RANGO):
-            dut.ALUb_i.value = valor_b
+            dut.ALUB.value = valor_b
             resultado = (valor_a << valor_b) & 0xF
             await Timer(1, 'ns')
-            assert dut.ALUresult_o.value == resultado
+            assert dut.ALUResult.value == resultado
 
 @cocotb.test()
 async def test_corrimiento_izquierda_unos(dut):
-    dut.ALUa_i.value = 0
-    dut.ALUb_i.value = 0
-    dut.ALUflagin_i.value = 1
-    dut.ALUcontrol_i.value = 8
+    dut.ALUA.value = 0
+    dut.ALUB.value = 0
+    dut.ALUFlagIn.value = 1
+    dut.ALUControl.value = 8
 
     for valor_a in range (RANGO):
-        dut.ALUa_i.value = valor_a
+        dut.ALUA.value = valor_a
         for valor_b in range (RANGO):
-            dut.ALUb_i.value = valor_b
+            dut.ALUB.value = valor_b
             resultado_esperado = ((valor_a << valor_b) | ((1 << valor_b) - 1)) & 0xF
             await Timer(1, 'ns')
-            assert dut.ALUresult_o.value == resultado_esperado
+            assert dut.ALUResult.value == resultado_esperado
 
 @cocotb.test()
 async def test_corrimiento_derecha_ceros(dut):
-    dut.ALUa_i.value = 0
-    dut.ALUb_i.value = 0
-    dut.ALUflagin_i.value = 0
-    dut.ALUcontrol_i.value = 9
+    dut.ALUA.value = 0
+    dut.ALUB.value = 0
+    dut.ALUFlagIn.value = 0
+    dut.ALUControl.value = 9
 
     for valor_a in range (RANGO):
-        dut.ALUa_i.value = valor_a
+        dut.ALUA.value = valor_a
         for valor_b in range (RANGO):
-            dut.ALUb_i.value = valor_b
+            dut.ALUB.value = valor_b
             resultado = (valor_a >> valor_b) & 0xF
             await Timer(1, 'ns')
-            assert dut.ALUresult_o.value == resultado
+            assert dut.ALUResult.value == resultado
 
 @cocotb.test()
 async def test_corrimiento_derecha_unos(dut):
-    dut.ALUa_i.value = 0
-    dut.ALUb_i.value = 0
-    dut.ALUflagin_i.value = 1
-    dut.ALUcontrol_i.value = 9
+    dut.ALUA.value = 0
+    dut.ALUB.value = 0
+    dut.ALUFlagIn.value = 1
+    dut.ALUControl.value = 9
 
     c = 0
 
     for valor_a in range (RANGO):
-        dut.ALUa_i.value = valor_a
+        dut.ALUA.value = valor_a
         c = 0
         for valor_b in range (RANGO):
-            dut.ALUb_i.value = valor_b
+            dut.ALUB.value = valor_b
             resultado = ((valor_a >> valor_b) | valores_b[c]) & 0xF
             await Timer(1, 'ns')
-            assert dut.ALUresult_o.value == resultado
+            assert dut.ALUResult.value == resultado
             if c <= 13:
                 c = c + 1
